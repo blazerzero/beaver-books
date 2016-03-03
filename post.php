278387f89dd745d6a9fd1bbc613dc0bbe4c317a8
julianweisbord <julianweisbord@gmail.com>
2016-03-03 15:42:02 -0800

    Added type column.
    Signed-off-by: julianweisbord <julianweisbord@gmail.com>

--- post.php
+++ post.php
@@ -1,5 +1,14 @@
 <?php include "./header.php" ?>
 
+<script>
+  function datepost(){
+    day_array = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var dt = new Date();
+    var time = dt.getHours() + ":" + dt.getMinutes() + " on " +day_array[dt.getDay()];
+    return time;
+  }
+</script>
+
 <?php
 
 /*function IsChecked($chkname,$value) {
@@ -12,7 +21,7 @@
 }*/
 
 if ($sql = $mysqli->prepare("INSERT INTO books (onid, subject, coursenum, title, author, price, isbn, cond, contact, address) VALUES (?,?,?,?,?,?,?,?,?,?)")) {
-  $sql->bind_param("ssisssssss", $onid, $subject, $coursenum, $title, $author, $price, $isbn, $condition, $contact, $address);
+  $sql->bind_param("ssisssssss", $onid,$date, $subject, $coursenum, $title, $author, $price, $isbn, $condition, $contact, $address);
 
   $onid = htmlspecialchars(checkAuth(false));
   $subject = $_POST["subject"];
@@ -22,7 +31,7 @@ if ($sql = $mysqli->prepare("INSERT INTO books (onid, subject, coursenum, title,
   $price = $_POST["price"];
   $isbn = $_POST["isbn"];
   $condition = "Excellent";
-
+  $date = echo "<script> datepost(); </script>";
   $contact = $_POST["contact"];
   $address = $_POST["address"];
 
