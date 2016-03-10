daeb2d7a392e6588bed110d0088c817268f285e4
blazerzero <habibelo@oregonstate.edu>
2016-03-09 18:53:12 -0800

    small change (adding dollar sign when there isn't one already there)

--- yourbooks.php
+++ yourbooks.php
@@ -45,7 +45,13 @@
   								<span class="date">Posted: <?php echo htmlspecialchars($obj->dateposted) ?> by <?php echo htmlspecialchars($obj->onid) ?></span>
   							</div>
 
-  							<h3><?php echo htmlspecialchars($obj->price) ?></h3>
+  							<?php if ($obj->price[0] != '$') { ?>
+                  <h3><?php echo '$' . htmlspecialchars($obj->price) ?></h3>
+                <?php }
+                else { ?>
+                  <h3><?php echo htmlspecialchars($obj->price) ?></h3>
+                <?php } ?>
+                
                 <h4>Condition: <?php echo htmlspecialchars($obj->cond) ?></h4>
   						</div>
               <div class="extra content">
